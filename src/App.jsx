765b2beedf0c0427451e955b7df237d98279e88a
saad200224 <saad19021994@outlook.com>
2023-09-13 14:26:15 +0600

    blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -6,6 +8,10 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect( () =>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+
+    },[])
     return (
         <div>
             
